96ac2a7ae559c9fb131ea9837cf02c83e5846d28
duanzhiqiang3 <duanzhiqiang1@jd.com>
2021-07-23 13:53:32 +0800

    去除无用包

--- src/main/java/com/dzq/concurrent/ScheduledThreadPoolExecutorTest.java
+++ src/main/java/com/dzq/concurrent/ScheduledThreadPoolExecutorTest.java
@@ -2,7 +2,6 @@ package com.dzq.concurrent;
 
 import org.junit.Test;
 
-import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
--- src/main/java/com/dzq/quartz/example1/HelloJob.java
+++ src/main/java/com/dzq/quartz/example1/HelloJob.java
@@ -3,8 +3,6 @@ package com.dzq.quartz.example1;
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 
 public class HelloJob implements Job {
